bce2460a7614677a86e4a8d1951707f98f0857a7
birmascctv <birmascctv@example.com>
2026-01-14 15:05:22 +0700

    updae vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
-        allowedHosts: ['.tail-net.ts.net']
+        allowedHosts: ['.tail-net.ts.net'],
+        hmr: {
+            clientPort: 443
+        }
     },
     plugins: [vue()]
 })
