029f260026394b97a34870a7d042a3ab4d3f4600
birmascctv <birmascctv@example.com>
2026-01-14 18:07:46 +0700

    change vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,23 +6,18 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    // This allows Tailscale to connect to the dev server
     allowedHosts: ['ubuntu-s-2vcpu-4gb-sgp1-01.tail79eba2.ts.net'],
     hmr: {
       clientPort: 443
     },
     proxy: {
-      // Proxying API calls to FastAPI/Backend
       '/api': {
         target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
+        changeOrigin: true
       },
-      // Proxying HLS stream to Media Server
       '/cam1': {
         target: 'http://127.0.0.1:8888',
-        changeOrigin: true,
-        secure: false,
+        changeOrigin: true
       }
     }
   }
