f8e289a01decef70f7cb46d2051d1b0a4699f72a
birmascctv <birmascctv@example.com>
2026-01-14 15:01:08 +0700

    funnel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        allowedHosts: ['.tail-net.ts.net']
+    },
     plugins: [vue()]
 })
