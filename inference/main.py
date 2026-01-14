ad314eade7d236e5dc59298c6a96fa687736f34c
birmascctv <birmascctv@example.com>
2026-01-14 11:19:01 +0700

    mainpy

--- inference/main.py
+++ inference/main.py
@@ -1,12 +1,20 @@
 import os, time, cv2, requests
 from ultralytics import YOLO
 
-RTSP_URL = os.getenv("RTSP_URL", "rtsp://178.128.105.184:8554/cam1")
-API_ENDPOINT = os.getenv("API_ENDPOINT", "http://localhost:8000/events")
-MODEL_PATH = os.getenv("MODEL_PATH", "./best.pt")
+#RTSP_URL = os.getenv("RTSP_URL", "rtsp://100.87.93.95:8554/cam1")
+#API_ENDPOINT = os.getenv("API_ENDPOINT", "http://localhost:8000/events")
+#MODEL_PATH = os.getenv("MODEL_PATH", "./best.pt")
+
+
+RTSP_URL = "rtsp://100.87.93.95:8554/cam1"
+RTSP_URLL = "rtsp://178.128.105.184:8554/cam1"
+API_ENDPOINT = "http://localhost:8000/events"
+MODEL_PATH = "./best.pt"
 
 #model = YOLO(MODEL_PATH)
 cap = cv2.VideoCapture(RTSP_URL)
+
+cap2 = cv2.VideoCapture(RTSP_URLL)
 print("Cap", cap)
 
 try:
@@ -24,6 +32,13 @@ while True:
         time.sleep(0.5)
         print("Failed to read frame from RTSP")
         continue
+
+    ok2, frame2 = cap2.read()
+    if not ok2:
+        time.sleep(0.5)
+        print("Failed to read frame from RTSP2")
+        continue
+
     res = model.predict(frame, imgsz=768, conf=0.3, iou=0.45, verbose=False)[0]
     for b in res.boxes:
         label = model.names[int(b.cls)]
