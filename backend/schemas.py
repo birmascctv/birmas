3864be6f21b3ac283a10962515649f8d23ff3311
birmascctv <birmascctv@example.com>
2026-03-02 17:19:11 +0700

    user login

--- backend/schemas.py
+++ backend/schemas.py
@@ -7,3 +7,16 @@ class Event(BaseModel):
     label: str
     confidence: float
     bbox: List[float]
+    
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    user_id: int
+    username: str
+
