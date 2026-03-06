4a92c4e6448adb2be71d526647af99e9300bd3bc
birmascctv <birmascctv@example.com>
2026-03-06 16:27:59 +0700

    baseurl server ip

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://10.0.0.1:8000/api'   // WireGuard IP + backend port
+  baseURL: 'http://170.64.149.147:8000/api'   // WireGuard IP + backend port
 })
 
 export default API
