b0af81382c445881504df2776455c894adf5e83b
birmascctv <birmascctv@example.com>
2026-03-09 15:46:25 +0700

    force ipv4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,20 +16,19 @@ export default defineConfig({
     allowedHosts: true, //['ubuntu-s-2vcpu-4gb-sgp1-01.tail79eba2.ts.net'], // ✅ explicitly allow your Funnel domain
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // ✅ matches your FastAPI backend
+        target: 'http://127.0.0.1:8000', // ✅ matches your FastAPI backend
         changeOrigin: true,
         secure: false,
       },
       '/ws': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         ws: true
       },
       '/stream': {
-        target: 'http://localhost:8888', // ✅ matches your MediaMTX stream
+        target: 'http://127.0.0.1:8888', // ✅ matches your MediaMTX stream
         changeOrigin: true,
         secure: false,
- //       ws: true,
         rewrite: (path) => path.replace(/^\/stream/, '')
       }
     }
